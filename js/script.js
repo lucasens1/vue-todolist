d083f6253c2cef94e95f0592587666db470eb2e3
lucasens1 <lucasensini1@gmail.com>
2024-04-20 16:03:08 +0200

    proj init

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const { createApp } = Vue;
+
+createApp({
+    data(){
+        return {
+            text : "",
+            done : true,
+            
+        }
+    },
+    created(){
+
+    },
+    methods : {
+
+    }
+}).mount('#app')
