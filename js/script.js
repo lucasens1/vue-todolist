1550658893c23ab7c80cefcae23893dc457ef3f3
lucasens1 <lucasensini1@gmail.com>
2024-04-20 16:48:41 +0200

    Milestone 2

--- js/script.js
+++ js/script.js
@@ -32,8 +32,11 @@ createApp({
 
     },
     methods : {
+        /**
+         * Funzione che aggiunge in maniera dinamia una nuova task al Toboolist, inizializzata false, perché è stata appena aggiunta quindi ancora da elaborare, funzione void, che semplicemente aggiorna l'array in Js e la lista in pagina
+         */
         addTask(){
-            this.tasks.push(this.newTask, this.toDoorNot);
+            this.tasks.push({ objTask : this.newTask, status : false });
         },
         /**
          * funzione che prende in entrata lo stato attuale della task, e l'indice nell'array della task selezionata, per dare lo stato Check/Uncheck inverto il valore che passo dello stato attuale
